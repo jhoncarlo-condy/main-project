f8ea44956a38ff38e36d28d742357e26be02214f
condy <jhoncarlo.condy@davidvenneit.com>
2022-08-18 20:13:23 +0800

    migrate users to module

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\User\Entities\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Model;
 
 class UserDatabaseSeeder extends Seeder
